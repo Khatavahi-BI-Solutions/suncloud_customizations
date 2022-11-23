fe94862da8bb83808eb1a7376fd842452a8ac651
Jigar Tarpara <jigartarpara68@gmail.com>
2022-11-23 12:19:47 +0530

    Chages

--- suncloud_customizations/api.py
+++ suncloud_customizations/api.py
@@ -31,3 +31,35 @@ def lead_query(doctype, txt, searchfield, start, page_len, filters):
     if data[0]['lead_name4']:
         finaldata.append([data[0]['lead_name4'],])    
     return finaldata
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def quotation_query(doctype, txt, searchfield, start, page_len, filters):
+    customer = filters['customer']
+    data = frappe.db.sql(""" 
+    select 
+        referencia_da_proposta,
+        referencia_da_proposta1,
+        referencia_da_proposta2,
+        referencia_da_proposta3,
+        referencia_da_proposta4 
+    from 
+        `tabCustomer` 
+    where 
+        name = %(customer)s
+    """,
+    {
+        'customer':customer
+    }, as_dict=True)
+    finaldata = []
+    if data[0]['referencia_da_proposta']:
+        finaldata.append([data[0]['referencia_da_proposta'],])
+    if data[0]['referencia_da_proposta1']:
+        finaldata.append([data[0]['referencia_da_proposta1'],])
+    if data[0]['referencia_da_proposta2']:
+        finaldata.append([data[0]['referencia_da_proposta2'],])
+    if data[0]['referencia_da_proposta3']:
+        finaldata.append([data[0]['referencia_da_proposta3'],])
+    if data[0]['referencia_da_proposta4']:
+        finaldata.append([data[0]['referencia_da_proposta4'],])    
+    return finaldata
