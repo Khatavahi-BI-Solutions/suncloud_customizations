07b2b1b6b8ca664beda1a1a177b9f5e67f3648ac
Jigar Tarpara <jigartarpara68@gmail.com>
2022-11-23 12:11:26 +0530

    Chages

--- suncloud_customizations/api.py
+++ suncloud_customizations/api.py
@@ -19,11 +19,15 @@ def lead_query(doctype, txt, searchfield, start, page_len, filters):
     {
         'customer':customer
     }, as_dict=True)
-    data = [
-        [data[0]['lead_name'],],
-        [data[0]['lead_name1'],],
-        [data[0]['lead_name2'],],
-        [data[0]['lead_name3'],],
-        [data[0]['lead_name4'],],
-    ]
+    data = []
+    if data[0]['lead_name']:
+        data.append(data[0]['lead_name'])
+    if data[0]['lead_name1']:
+        data.append(data[0]['lead_name1'])
+    if data[0]['lead_name2']:
+        data.append(data[0]['lead_name2'])
+    if data[0]['lead_name3']:
+        data.append(data[0]['lead_name3'])
+    if data[0]['lead_name4']:
+        data.append(data[0]['lead_name4'])    
     return data
